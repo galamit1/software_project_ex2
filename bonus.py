c74ee0397ad40293e54991b0f6fd87cb24c893ba
Ben Wattelman <benwattelman6@gmail.com>
2021-06-13 23:14:04 +0300

    Updated bonus.py code so it annotates

--- bonus.py
+++ bonus.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn import datasets
+from kneed import KneeLocator
 
 
 def main():
@@ -18,7 +19,19 @@ def main():
                     np.power(np.subtract(np.multiply(point, np.ones((centroids.shape[0], points.shape[1]))), centroids),
                              2), axis=1)))
         plot = np.append(plot, distances.sum())
+    
+    kn = KneeLocator([i for i in range(1,11)], plot, curve='convex', direction='decreasing')
+    elbow = (kn.knee, plot[kn.knee])
+    plt.annotate('Elbow Point', xy=elbow, xytext=(60, 60),
+            textcoords='offset points',
+            color='b', size='large',
+            arrowprops=dict(
+                arrowstyle='simple,tail_width=0.3,head_width=0.8,head_length=0.8',
+                facecolor='b', shrinkB=20))
+
     plt.plot(ks, plot)
+    plt.xlabel('K')
+    plt.ylabel('Average Dispersion')
     plt.savefig('elbow.png')
 
 
